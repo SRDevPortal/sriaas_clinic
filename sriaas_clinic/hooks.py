02e9aec262c23ea14291dfd6c356f29862997bd5
SRDevPortal <webdevelopersriaas@gmail.com>
2026-03-24 16:33:10 +0530

    chore(hooks): update app hooks configuration

--- sriaas_clinic/hooks.py
+++ sriaas_clinic/hooks.py
@@ -207,14 +207,14 @@ doc_events = {
     # "Purchase Order": {
     #     "before_submit": "sriaas_clinic.api.purchase_order.create_batches_before_submit"
     # },
-    # "File": {
-    #     "after_insert": [
-    #         "sriaas_clinic.api.s3.file_hooks.handle_file_after_insert",
-    #     ],
-    #     "on_trash": [
-    #         "sriaas_clinic.api.s3.file_hooks.handle_file_on_trash",
-    #     ],
-    # }
+    "File": {
+        "after_insert": [
+            "sriaas_clinic.api.s3.file_hooks.handle_file_after_insert",
+        ],
+        "on_trash": [
+            "sriaas_clinic.api.s3.file_hooks.handle_file_on_trash",
+        ],
+    }
 }
 
 doctype_js = {

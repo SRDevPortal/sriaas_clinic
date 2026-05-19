a45ebf5223fba8862a3c2b287c995e61c098e2bf
SRDevPortal <webdevelopersriaas@gmail.com>
2026-05-19 16:08:34 +0530

    chore: update app hooks configuration

--- sriaas_clinic/hooks.py
+++ sriaas_clinic/hooks.py
@@ -4,6 +4,7 @@ app_publisher = "SRIAAS"
 app_description = "Clinic customizations packaged as clean installable/uninstallable app."
 app_email = "webdevelopersriaas@gmail.com"
 app_license = "mit"
+required_apps = ["sriaas_role_permissions"]
 
 # Installation
 # before_install = "sriaas_clinic.install.before_install"
